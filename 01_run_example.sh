6a6a855ae80ccfb24ea617bfa3f54bfe62fb701a
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2023-05-23 01:28:55 +0900

    WIP

--- 01_run_example.sh
+++ 01_run_example.sh
@@ -1,1 +1,2 @@
-../pin-3.27-98718-gbeaa5d51e-gcc-linux/pin -t ./obj-intel64/CodeCoverage.so -- examples/c_function_call/cov_sample 1 2
+#../pin-3.27-98718-gbeaa5d51e-gcc-linux/pin -t ./obj-intel64/CodeCoverage.so -- examples/c_function_call/cov_sample 1 2
+../pin-3.27-98718-gbeaa5d51e-gcc-linux/pin -t ./obj-intel64/CodeCoverage.so -- examples/simple_branch/simple_branch
