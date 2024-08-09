f3497f601cec72d7d2c29f50859718295692b704
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2024-08-09 11:53:11 +0900

    update to 3.28 TODO Segmentation fault occurs in versions after 3.29

--- 00_setup.sh
+++ 00_setup.sh
@@ -1,8 +1,8 @@
 # download and extract intel pin
 pushd .
 cd ..
-wget https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.27-98718-gbeaa5d51e-gcc-linux.tar.gz
-tar xf pin-3.27-98718-gbeaa5d51e-gcc-linux.tar.gz
+wget https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.28-98749-g6643ecee5-gcc-linux.tar.gz
+tar xf pin-3.28-98749-g6643ecee5-gcc-linux.tar.gz
 popd
 
 # build CodeCoverage(this repository code)
